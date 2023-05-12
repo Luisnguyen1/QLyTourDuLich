5b803ea1e315ea5e1e585c1c254e41d3d805ea17
conganh12345 <anhvca1234@gmail.com>
2023-05-12 10:40:31 +0700

    update

--- src/main/java/DTo/TaiKhoan.java
+++ src/main/java/DTo/TaiKhoan.java
@@ -6,23 +6,18 @@ import java.util.ArrayList;
 public class TaiKhoan {
     private String matk;
     private String matkhau;
-    private int manv;
-    private String quyentruycap;
+ 
     
     public TaiKhoan() {
     }
 
-    public TaiKhoan(String matk, String matkhau, int manv, String quyentruycap) {
+    public TaiKhoan(String matk, String matkhau) {
         this.matk = matk;
         this.matkhau = matkhau;
-        this.manv = manv;
-        this.quyentruycap = quyentruycap;
+     
     }
 
-    TaiKhoan(String matk, String matkhau) {
-        this.matk = matk;
-        this.matkhau = matkhau;
-       }
+  
 
     public String getMatk() {
         return matk;
@@ -40,29 +35,10 @@ public class TaiKhoan {
         this.matkhau = matkhau;
     }
 
-    public int getManv() {
-        return manv;
-    }
-
-    public void setManv(int manv) {
-        this.manv = manv;
-    }
-
-    public String getQuyentruycap() {
-        return quyentruycap;
-    }
-
-    public void setQuyentruycap(String quyentruycap) {
-        this.quyentruycap = quyentruycap;
-    }
+  
 
 
-
-    @Override
-    public String toString() {
-        return "TaiKhoan{" + "quyentruycap=" + quyentruycap + '}';
-    }
-
+  
     
         
 }
